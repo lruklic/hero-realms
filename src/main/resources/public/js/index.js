3241eec8426e84f39d353590b1334d03af4293f3
Luka Ruklić <ruklic.luka@gmail.com>
2017-07-17 11:17:08 +0200

    Added market and player tiles with content, implemented acquire method and animation

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -26,19 +26,35 @@ function gui() {
             $("#opponent-deck-image-text").css("opacity", 1);
         });
     
-    $(".opponent-playable-nonpermanent").click(function() {
+    $(".scalable").click(function() {
         if ($(this).hasClass('scaled')) {
             var transform = "scale(1)";
             if ($(this).hasClass("rotated")) {
                 transform += "rotate(90deg)";
             }
-            $(this).removeClass('scaled').css("transform", transform);
+            d3.select(this).classed('scaled', false)
+                .style("transform", transform)
+                .transition().duration(200)
+                .style("z-index", 99);
         } else {
-            $(this).addClass("scaled").css("transform", "scale(3.5)");        
+            var maxZIndex = 0;
+            d3.selectAll(".scalable").each(function() {
+                var zIndex = Number(d3.select(this).style("z-index"));
+                if (zIndex > maxZIndex) maxZIndex = zIndex;
+            });
+            
+            d3.select(this).classed("scaled", true)
+                .style("transform", "scale(3.5)")
+                .style("z-index", Number(maxZIndex) + 1);        
         }       
     });
 
-    d3.selectAll(".opponent-playable-nonpermanent.mouseenter-trigger").on("mouseenter", function() {
+    d3.selectAll(".opponent-playable-nonpermanent.mouseenter-trigger, .player-playable-nonpermanent.mouseenter-trigger").on("mouseenter", function() {
+        
+        if (!d3.select(this).classed("mouseenter-trigger")) {
+            return;
+        }
+
         cardAbilityHover.id = d3.select(this).attr('id');
         cardAbilityHover.active = true;
         d3.select(this.parentNode).select("div")
@@ -56,7 +72,7 @@ function gui() {
 
     d3.selectAll(".ability-picker").on("click", function() {
         var id = d3.select(this.parentNode).attr("id").split("-")[2];
-        tapAndActivate(id);
+        tapAndActivate("opponent", id);
         update("opponent", "combat", 5);
     });
 
@@ -87,12 +103,53 @@ function update(target, category, value) {
     });
 }
 
-function tapAndActivate(id) {
-    var cardObject = d3.select("#opponent-card-" + id + " img")
+function tapAndActivate(target, id) {
+    var cardObject = d3.select("#" + target + "-card-" + id + " img")
     
+    cardObject.classed("mouseenter-trigger", false);
+
+    d3.select(cardObject.node().parentNode)
+        .select(".ability-picker")
+        .style("display", "none");
+
     cardObject.transition().duration(300)
         .style("transform", "rotate(90deg)");
 
     cardObject.classed("rotated", true);
 }
 
+function acquire() {
+    d3.select(".flipper").style("transform", "rotateY(180deg)");
+    
+    //var clone = $(".flipper").clone(); 
+    var offset = $(".flipper img").offset(); 
+    
+/*    clone.attr("id", "animation").css({
+        "position" : "absolute",
+        "left"     : offset.left + 150,
+        "top"      : offset.top,
+        "opacity" : 0
+    }).appendTo($(".container"));*/
+
+    var endPoint = $("#player-discard-pile-image").offset();
+
+    var animation = d3.select("#animation");
+
+    animation
+        .style("top", offset.top + "px")
+        .style("left", offset.left + "px")
+        .transition().delay(700).duration(1)
+        .style("opacity", 1);
+    
+    //console.log(endPoint.top + " " + endPoint.left);
+    animation.transition().delay(710).duration(500)
+        .style("top", endPoint.top + "px")
+        .style("left", endPoint.left + "px")
+        .attr("width", 114)
+        .attr("height", 75);
+
+    animation.transition().delay(710 + 510).duration(100)
+        .style("opacity", 0)
+
+    
+}
