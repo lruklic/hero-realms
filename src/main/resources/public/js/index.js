c90e3e18b0411e57e9ab4431b6cf9e7ef3ef7af0
Luka Ruklić <ruklic.luka@gmail.com>
2017-07-23 14:20:04 +0200

    Added player hand with animated card play method

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -44,6 +44,14 @@ function gui() {
         acquire(id);
     });
 
+    $(".hand-card")
+        .mouseenter(function() {
+            $(this).css("transform", "translate(0px, -20px)")
+        })
+        .mouseleave(function() {
+            $(this).css("transform", "translate(0px, 0px)")
+        })
+
     $(".container").on("contextmenu", ".scalable", function() {
         if ($(this).hasClass('scaled')) {
             var transform = "scale(1)";
@@ -145,6 +153,75 @@ function tapAndActivate(target, id) {
     cardObject.classed("rotated", true);
 }
 
+function playCard(id) {
+    // check if permanent or nonpermanent
+
+    var animation = d3.select("#animation");
+    var playedCard = d3.select("#hand-" + id);
+    var playedCardImg = playedCard.select("image").attr("xlink:href");
+
+    d3.select("#animation image")
+        .attr("xlink:href", playedCardImg);
+
+    animation
+        .classed("shadow", false)
+        .style("display", "inherit");
+
+    var selection = d3.selectAll(".player-playable-field");
+    var oldSelectionSize = selection.size();
+    var percentageSize = 100 / (oldSelectionSize + 1);
+
+    $(".player-permanent-board")
+        .append('<div class="player-playable-field player-playable-' + (oldSelectionSize + 1) + '" style="width: 0%">' +
+            '<svg height="126px" width="90px" class="scalable transparent0"><image xlink:href="' + playedCardImg + '"/></svg>' + '</div>')
+
+    d3.selectAll(".player-playable-field").transition().duration(500).style("width", percentageSize + "%");
+
+    var lastElement = d3.select(".player-playable-" + (oldSelectionSize + 1) + " svg");
+    lastElement.transition().duration(500).on("end", function() { 
+        var endPoint = $(".player-playable-" + (oldSelectionSize + 1) + " svg").offset();
+
+        animation
+            .transition().delay(510).duration(500)
+            .style("top", endPoint.top + "px")
+            .style("left", endPoint.left + "px")
+            .attr("width", lastElement.attr("width"))
+            .attr("height", lastElement.attr("height"));
+        
+        setTimeout(function() {
+            animation.style("display", "none");
+        }, 1400);
+/*        animation
+            .transition().delay(1050).duration(1)
+            //.style("display", "none");
+            .style("opacity", 0);*/
+        setTimeout(function() {
+            lastElement.style("opacity", 1);
+        }, 1010);
+/*        lastElement
+            .transition().delay(1020).duration(1)
+            .style("opacity", 1);*/
+
+    })
+
+    var startPoint = $(playedCard.node()).offset();
+
+    animation
+        .attr("width", playedCard.style("width"))
+        .attr("height", playedCard.style("height"))    
+        .attr("src", playedCardImg)    
+        .style("top", startPoint.top + "px")
+        .style("left", startPoint.left + "px") 
+        .style("opacity", 1);
+
+    playedCard
+        .style("opacity", 0)
+
+/*    $("#player-card-4")
+        .append('<img class="shadow player-playable-nonpermanent scalable mouseenter-trigger" src="images/koska.jpg" alt="Deck" height="114" width="75">')
+        .addClass("column")*/
+}
+
 function acquire(id) {
     d3.select("#market-container-" + id + " .flipper").style("transform", "rotateY(180deg)");
 
@@ -153,12 +230,19 @@ function acquire(id) {
     var endPoint = $("#player-discard-pile-image").offset();
 
     var animation = d3.select("#animation");
+    var animationImage = d3.select("#animation image");
 
     animation
         .style("top", offset.top + "px")
         .style("left", offset.left + "px")
+        .attr("width", 150)
+        .attr("height", 98)
         .transition().delay(700).duration(1)
-        .style("opacity", 1);
+        .style("opacity", 1)
+        .style("display", "inherit")
+
+    animationImage
+        .attr("xlink:href", "images/back-rotate-right.jpg")
     
     //console.log(endPoint.top + " " + endPoint.left);
     animation.transition().delay(710).duration(500)
@@ -166,6 +250,10 @@ function acquire(id) {
         .style("left", endPoint.left + "px")
         .attr("width", 114)
         .attr("height", 75);
+
+/*    animationImage.transition().delay(710).duration(500)
+        .attr("width", 114)
+        .attr("height", 75);*/
 
     animation.transition().delay(1220).duration(100)
         .style("opacity", 0)
