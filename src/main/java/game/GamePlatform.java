d8f0eda1c3ac4ed2739e36f57d3d800d9acecf23
Luka Ruklić <ruklic.luka@gmail.com>
2017-08-31 00:00:53 +0200

    Added GamePlatform class as storage for active matches

---
+++ src/main/java/game/GamePlatform.java
@@ -0,0 +1,49 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import model.entities.Match;
+import model.entities.Player;
+
+/**
+ * Class that stores all active matches.
+ * 
+ * @author LukaRuklic
+ *
+ */
+
+public class GamePlatform {
+	
+	private GamePlatform() {};
+	
+	public static List<Match> activeMatches = new ArrayList<>();
+	
+	public static void addMatch(Match match) {
+		activeMatches.add(match);
+	}
+	
+	public static Match getMatchById(String uuid) {
+		for (Match match : activeMatches) {
+			if (match.getUUID().equals(uuid)) {
+				return match;
+			}
+		}
+		
+		return null;
+	}
+	
+	public static Match findPlayerInMatch(String username) {
+		for (Match match : activeMatches) {
+			for (Map.Entry<String, Player> entry : match.getGame().getPlayers().entrySet()) {
+				if (username.equals(entry.getValue().getName())) {
+					return match;
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+}
